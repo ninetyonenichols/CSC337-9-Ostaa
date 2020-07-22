cc6419cadd9c04096b6c91eab71de73c18956d2a
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-22 07:25:36 +0000

    Added MongoDB models.

--- ostaa/server.js
+++ ostaa/server.js
@@ -4,7 +4,6 @@
  * Class: CSC337
  */
 
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -14,7 +13,7 @@ const port = 80;
 
 const app = express();
 const db = mongoose.connection;
-const mongoDBURL = 'mongodb://localhost/dbnamehere';
+const mongoDBURL = 'mongodb://localhost/ostaa';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,11 +21,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(mongoDBURL, { useNewUrlParser: true });
 db.on('error', console.error.bind(console, 'MongoDB connection error.'));
 
-// TODO: insert schemas
+var Schema = mongoose.Schema;
 
+// Items
+var ItemSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+  price: Number,
+  stat: String 
+  });
+var Item = mongoose.model('Item', ItemSchema);
 
+// Users
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+  listings: [Schema.Types.ObjectId],
+  purchases: [Schema.Types.ObjectId] 
+  })
+var User = mongoose.model('User', UserSchema);
 
 app
   .use(express.static('public_html'))
+  .get('/get/users', (req, res) => console.log("asdf"))
+  .get('/get/items', (req, res) => console.log("asdf"))
+  .get('/get/listings/:username', (req, res) => console.log("asdf"))
+  .get('/get/purchases/:username', (req, res) => console.log("asdf"))
+  .get('/search/users/:keyword', (req, res) => console.log("asdf"))
+  .get('/search/items/:keyword', (req, res) => console.log("asdf"))
+  .post('/add/user', (req, res) => console.log("asdf"))
+  .post('/add/item/:username', (req, res) => console.log("asdf"))
   .all('*', (req, res) => res.redirect('/'))
   .listen(port, () => console.log('App listening.'))
