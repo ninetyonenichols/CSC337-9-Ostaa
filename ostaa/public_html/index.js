51841c63077a7dc4846d40873195db297dd83e1c
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-22 10:44:47 +0000

    Finished AJAX.

--- ostaa/public_html/index.js
+++ ostaa/public_html/index.js
@@ -1,12 +1,32 @@
 /*
- * This file
+ * This file allows users to create accounts / add items by sending AJAX
+     requests to the server.
  * Author: Justin Nichols
  * Class: CSC337
  */
 
 function addUser() {
+  $.ajax({
+    url: '/add/user',
+    method: 'POST',
+    data: {  
+      username: $('#username').val(),
+      password: $('#password').val(),
+    }
+  });
 }
 
 function addItem() {
-  
+  $.ajax({
+    url: `add/item/${$('username').val()}`,
+    method: 'POST',
+    data: {  
+      title: $('title').val();  
+      desc: $('desc').val();  
+      image: $('image').val();  
+      price: $('price').val();  
+      avail: $('avail').val();  
+      seller: $('seller').val();  
+    }
+  });
 }
